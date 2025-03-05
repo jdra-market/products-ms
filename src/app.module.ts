f2d5fe3a632816c40c02f5722e4fcbe5fe1402c6
Jairo Rey <jairodanielrey@gmail.com>
2025-03-04 23:21:13 -0500

    Actualizar configuración de TypeORM para usar la URL de la base de datos de productos desde el entorno

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,17 +1,14 @@
 import { Module } from '@nestjs/common';
 import { ProductsModule } from './products/products.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { envs } from './config';
 
 @Module({
   imports: [
     ProductsModule,
     TypeOrmModule.forRoot({
       type: 'postgres',
-      host: 'products-db',
-      port: 5432,
-      username: 'postgres',
-      password: '123456',
-      database: 'productsdb',
+      url: envs.productsDatabaseUrl,
       entities: ['dist/**/*.entity{.ts,.js}'],
       synchronize: true,
       retryDelay: 3000,
